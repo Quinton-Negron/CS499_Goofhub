5eb1bc1435e116923cbc3c078b77a10f88f42754
Mei Tak <wattevah@gmail.com>
2020-09-26 18:08:11 -0400

    Create navbar and empty pages

---
+++ fontend/src/Submitjoke.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SubmitJoke() {
+    return (
+        <div className='SubmitJoke'>
+            <h1>Submit a Joke</h1>
+        </div>
+    );
+}
+
+export default SubmitJoke;
