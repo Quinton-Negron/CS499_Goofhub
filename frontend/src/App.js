ad62d26d03f53d5f6d9c4570c64a612c93944ae3
Mei Tak <wattevah@gmail.com>
2020-09-26 20:44:19 -0400

    rename folder

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Home';
+import SubmitJoke from './Submitjoke';
+import LogIn from './Login';
+import SignUp from './Signup';
+import Search from './Search';
+import Navibar from './components/Navibar';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navibar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/submitjoke" component={SubmitJoke} />
+            <Route path="/login" component={LogIn} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/search" component={Search} />
+          </Switch>
+     
+      </div>
+    </Router>
+    
+  );
+  
+}
+
+export default App;
