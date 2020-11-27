86396512326915d25f8b8347a0e2a55b9d378b34
Mei Tak <wattevah@gmail.com>
2020-11-26 19:27:53 -0500

    add email/password change

--- frontend/src/Profile.js
+++ frontend/src/Profile.js
@@ -13,10 +13,16 @@ const Profile = () => {
   const users = useGetUser('users',currentUserId);
 
   const [name, setName] = useState("");
+  const [newEmail, setnewEmail] = useState("");
+  const [currentPassword1, setcurrentPassword1] = useState("");
+  const [newPassword, setnewPassword] = useState("");
+  const [currentPassword, setcurrentPassword] = useState("");
+  
+  const regex = /^\s*$/;//regular expression for blank spaces
 
   //updates username and displayName
   function editName(users) {
-    const regex = /^\s*$/;//regular expression for blank spaces
+    
     //if input has blank spaces use original displayName, othewise use new input
     const newName = (regex.test(name) === true) ? currentUser.displayName : name;
     
@@ -31,76 +37,119 @@ const Profile = () => {
       user.updateProfile({
           displayName: newName
           });
-    })
-    .then(() => {
+    }).then(() => {
         alert("Username has changed. Refresh your page for latest update.");
-    })
-    .catch((error) => {
-        alert(error.message);
+    }).catch((error) => {alert(error.message);
     });
 
     setName("");
       
 };
 
+  //needs to reauthenticate when changing email or password
+  function reauthenticate(currentPassword) {
+    const user = firebase.auth().currentUser;
+    const cred = firebase.auth.EmailAuthProvider.credential(
+        user.email, currentPassword);
+    return user.reauthenticateWithCredential(cred);
+  }
+
+  //updates email in firestore and authentication
+  function changeEmail(users, currentPassword1, newEmail) {
+    const regex1 = /[\w\d.-]+@[\w\d.-]+\.[\w\d.-]+/;//regular expression for valid email
+    //if input has blank spaces display an alert, othewise use new input
+    const nEmail = (regex1.test(newEmail) === false || regex.test(newEmail) === true) ? alert("Incorrect email format!") : newEmail;
+    
+    const updateEmail = {
+      email: nEmail,
+    };
+    reauthenticate(currentPassword1)
+    .then(() => {//creates user in firestore
+      return firebase.firestore().collection("users")
+      .doc(users.id)
+      .update(updateEmail)//update in firestore
+    }).then(() => {
+      const user = firebase.auth().currentUser;
+      user.updateEmail(nEmail).then(() => {
+       alert("Email updated!");
+      }).catch((error) => { alert(error.message); });
+    }).catch((error) => {console.log(error) });
+    setcurrentPassword1("");
+    setnewEmail("");
+  }
+
+  //change password 
+  function changePassword(currentPassword, newPassword) {
+    const nPassword = (regex.test(newPassword) === true) ? null : newPassword;
+    
+    reauthenticate(currentPassword)
+      .then(() => {
+        const user = firebase.auth().currentUser;
+        user.updatePassword(nPassword).then(() => {
+          alert("Password updated!");
+        }).catch((error) => { alert(error.message); });
+    }).catch(() => { alert("Password is badly formatted. It must be 6-20 characters long!"); });
+    setcurrentPassword("");
+    setnewPassword("");
+  }
+
   return (
-    currentUser && (  
-      
+    currentUser && ( 
       <Row lg={3}>
       <Col lg={2}></Col>
       <Col lg={8}>
       <Card className="proCard">
+      
       {users.map((items) => (
       <Row key={items.id}>
         <Col md={3} className="mt-5 align-items-center ">
           <img
             src={currentUser.photoURL}
-            alt="Profile"         
-            className="rounded-circle proimg-fluid "
+            alt="Profile"
+            className="rounded-circle proimg-fluid"
           />
           <div className="btn-xlg proAddBtn">
           <i className="fas fa-plus-circle"></i></div>
-        
-          <Row>
+        <Row>
             <Col  className="mt-5 align-items-center profile-header">
               <Card className="proSideCard">
                 <Card.Title className="proSideName">{items.first} {items.last}</Card.Title>
                 <Card.Text className="proSideUser">{items.username}</Card.Text>
-                <Card.Text className="proSideEmail">{currentUser.email}</Card.Text>
+                <Card.Text className="proSideEmail">{items.email}</Card.Text>
               </Card>
             </Col>
           </Row>
         </Col>
         <Col md>
       <Card.Body className="proCardBody">
-        <Card.Title className="proTitle">ACCOUNT SETTINGS</Card.Title>    
-        <Form className="proForm">
+        <Card.Title className="proTitle">ACCOUNT SETTINGS</Card.Title>  
+        <Form className="proForm" >
           <Form.Row className="progroup proLineTop">
-          <Form.Group as={Col} >
-            <Form.Label  className="proWhite">NAME</Form.Label>
-          </Form.Group>
-          <Form.Group as={Col} >
-            <Form.Label  className="proText" >{items.first} {items.last}</Form.Label>
-          </Form.Group>
-          <Form.Group as={Col}></Form.Group>
+            <Form.Group as={Col} >
+              <Form.Label  className="proWhite">NAME</Form.Label>
+            </Form.Group>
+            <Form.Group as={Col} >
+              <Form.Label  className="proText" >{items.first} {items.last}</Form.Label>
+            </Form.Group>
+            <Form.Group as={Col}></Form.Group>
           </Form.Row>
 
           <Form.Row className="progroup">
-          <Form.Group as={Col} >
-            <Form.Label className="proWhite">DATE OF BIRTH</Form.Label>
-          </Form.Group>
-          <Form.Group as={Col}>
-              <Form.Label  className="proText">{items.dob}</Form.Label>
-          </Form.Group>
-          <Form.Group as={Col} ></Form.Group>
+            <Form.Group as={Col} >
+              <Form.Label className="proWhite">DATE OF BIRTH</Form.Label>
+            </Form.Group>
+            <Form.Group as={Col}>
+                <Form.Label  className="proText">{items.dob}</Form.Label>
+            </Form.Group>
+            <Form.Group as={Col} ></Form.Group>
           </Form.Row>
-
+               
           <Form.Row className="progroup ">
-          <Form.Group  as={Col} >
-            <Form.Label className="proWhite" >USERNAME</Form.Label>
-          </Form.Group>
-          <Form.Group as={Col} >
-          <Form.Control required 
+            <Form.Group  as={Col} >
+              <Form.Label className="proWhite" >USERNAME CHANGE</Form.Label>
+            </Form.Group>
+            <Form.Group as={Col} >
+              <Form.Control required 
                 className="procontrol proText" 
                 value={name}
                 onChange={(e) => setName(e.target.value)}
@@ -113,32 +162,63 @@ const Profile = () => {
                 <i className="fas fa-pen"></i></div>
             </Form.Group>
           </Form.Row>
+          
 
           <Form.Row className="progroup">
-          <Form.Group as={Col} controlId="formPlaintextEmail">
-            <Form.Label className="proWhite">EMAIL</Form.Label>
-          </Form.Group>  
-          <Form.Group as={Col}>
-              <Form.Control required className="procontrol proText" size="sm" type="email" placeholder={currentUser.email}/>
-          </Form.Group>
-          <Form.Group as={Col}>
-              <div className="btn-xlg profas">
-              <i className="fas fa-pen"></i></div>
-          </Form.Group>
+            <Form.Group as={Col} >
+              <Form.Label className="proWhite1">EMAIL CHANGE</Form.Label>
+            </Form.Group>  
+            <Form.Group as={Col} >
+                <Form.Control required 
+                  controlid="formPlaintextEmail"
+                  className="procontrol proText" 
+                  value={newEmail}
+                  onChange={(e) => setnewEmail(e.target.value)}
+                  size="sm" 
+                  type="email" 
+                  placeholder={items.email}/>
+           
+                <Form.Control required 
+                  controlid="formPlaintextPassword"
+                  className="procontrol proText1" 
+                  value={currentPassword1}
+                  onChange={(e) => setcurrentPassword1(e.target.value)}
+                  size="sm" 
+                  type="password" 
+                  placeholder="Current Password"/>
+            </Form.Group>
+            <Form.Group as={Col}>
+                <div type="submit" onClick={() => changeEmail(items,currentPassword1,newEmail)} className="btn-xlg profas">
+                <i className="fas fa-pen"></i></div>
+            </Form.Group>
           </Form.Row>
 
           <Form.Row className="proLineBot">
-
-          <Form.Group as={Col}  controlId="formPlaintextPassword">
-            <Form.Label className="proWhite" >NEW PASSWORD</Form.Label>
-          </Form.Group>  
-          <Form.Group as={Col}>
-              <Form.Control required className="procontrol proText" size="sm" type="password" placeholder="......" />
-          </Form.Group>  
-          <Form.Group as={Col}>
-              <div className="btn-xlg profas">
-              <i className=" fas fa-pen"></i></div>  
-          </Form.Group>
+            <Form.Group as={Col}  >
+              <Form.Label className="proWhite1" >PASSWORD CHANGE</Form.Label>
+            </Form.Group>  
+            <Form.Group as={Col} >
+            <Form.Control required 
+                controlid="formPlaintextPassword1"
+                className="procontrol proText" 
+                value={currentPassword}
+                onChange={(e) => setcurrentPassword(e.target.value)}
+                size="sm" 
+                type="password" 
+                placeholder="Current Password"/>
+              <Form.Control required 
+                controlid="formPlaintextPassword2"
+                className="procontrol proText1" 
+                value={newPassword}
+                onChange={(e) => setnewPassword(e.target.value)}
+                size="sm" 
+                type="password" 
+                placeholder="New Password" />
+            </Form.Group>  
+            <Form.Group as={Col}>
+                <div type="submit" onClick={() => changePassword(currentPassword,newPassword)} className="btn-xlg profas">
+                <i className="fas fa-pen"></i></div>  
+            </Form.Group>
           </Form.Row>
         </Form>
       </Card.Body>
@@ -146,9 +226,7 @@ const Profile = () => {
         </Col>
       </Row>
       ))}
-      {/*<Row>
-        {JSON.stringify(currentUser, null, 2)}
-      </Row>*/}
+     
       </Card>
     </Col>
     <Col lg={2}></Col>
