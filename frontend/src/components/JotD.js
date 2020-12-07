8f9621ceb2d093304bc3accc00d95724ad9987cf
Ricardo Daniels <70983005+LuteLuthier@users.noreply.github.com>
2020-12-06 19:26:13 -0500

    Add files via upload
    added filter to not show unreleased jokes

--- frontend/src/components/JotD.js
+++ frontend/src/components/JotD.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './JotD.css';
-import { Card, Container, ResponsiveEmbed } from 'react-bootstrap';
+import { Card, Container, ResponsiveEmbed} from 'react-bootstrap';
 import firebase from "../firebase/firebase";
 
 const db = firebase.firestore();
-const JotD = props => { 
+const JotD = props => {
 
     function useFetch() {
         const [jokes, setJokes] = useState([]);
@@ -22,22 +22,24 @@ const JotD = props => {
         }, [])
         return jokes
     }
+
     const jokes = useFetch();
     const no18Jokes = jokes.filter(word => word.category.some(data => data !== '18+'));
-    const items = no18Jokes.map((data) => {
-    switch (data.type) {
-        case data.type = 'image':
-            return (<Card.Img className="jokebox2" variant="top" src={data.content} />)
-        case data.type = 'video':
-            return (<><div style={{ width: 'auto', height: 'auto' }}>
-                        <ResponsiveEmbed aspectRatio="16by9">
-                            <embed type="image/svg+xml" src={data.content} />
-                        </ResponsiveEmbed>
-                    </div></>)
-        case data.type = 'text':
-            return (<div>{data.content}</div>)
-        default:
-            return (<div>{data.content}</div>)
+    const unreleasedJokes = no18Jokes.filter(word => word.release !== false);
+    const items = unreleasedJokes.map((data) => {
+        switch (data.type) {
+            case data.type = 'image':
+                return (<Card.Img className="jokebox2" variant="top" src={data.content} />)
+            case data.type = 'video':
+                return (<><div style={{ width: 'auto', height: 'auto' }}>
+                    <ResponsiveEmbed aspectRatio="16by9">
+                        <embed type="image/svg+xml" src={data.content} />
+                    </ResponsiveEmbed>
+                </div></>)
+            case data.type = 'text':
+                return (<div>{data.content}</div>)
+            default:
+                return (<div>{data.content}</div>)
         }
     })
     var d = new Date();          //JotD indexer algo
@@ -48,30 +50,33 @@ const JotD = props => {
     const month = m.getMonth();
     const year = y.getFullYear();
     const random = day + month + year;
-    
+
     const len = items.length;
-    const JotdIndex = random % len;
-    const item = items[JotdIndex];
+    var JotdIndex = random % len;
+    var item = items[JotdIndex];
+
+
+   
 
     return (
-    <Card className="jotd">
-        <Container className ="borderjoke">
-            <Card.Body>
-                <Card.Title><center><h1>Joke of the Day!  <img
-                alt=""
-                src="https://firebasestorage.googleapis.com/v0/b/goofhub-team.appspot.com/o/app%2Flaugh.png?alt=media&token=e12ba2c4-e1a3-4a5e-88bc-3f7b28fca8b9"
-                width="37"
-                height="37"
-                className=" d-inline-block align-center" 
-            /> {' '}</h1></center>
-            <div className="jotdivider divider"></div>
-            <div className='subheader1'>
-                <center><i> {item} </i></center>
-                                 
-            </div>
-            </Card.Title>
-            </Card.Body></Container>
-            </Card>
+        <Card className="jotd">
+            <Container className="borderjoke">
+                <Card.Body>
+                    <Card.Title><center><h1>Joke of the Day!  <img
+                        alt=""
+                        src="https://firebasestorage.googleapis.com/v0/b/goofhub-team.appspot.com/o/app%2Flaugh.png?alt=media&token=e12ba2c4-e1a3-4a5e-88bc-3f7b28fca8b9"
+                        width="37"
+                        height="37"
+                        className=" d-inline-block align-center"
+                    /> {' '}</h1></center>
+                        <div className="jotdivider divider"></div>
+                        <div className='subheader1'>
+                            <center><i> {item} </i></center>
+
+                        </div>
+                    </Card.Title>
+                </Card.Body></Container>
+        </Card>
     )
 }
 export default JotD;
